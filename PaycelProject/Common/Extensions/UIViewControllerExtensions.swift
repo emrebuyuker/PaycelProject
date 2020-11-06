a92ef63b7658e6630082100daff07f2f43787558
Emre BUYUKER <emre.buyuker@veripark.com>
2020-11-06 23:03:31 +0300

    Added extensions on UIViewController.

---
+++ PaycelProject/Common/Extensions/UIViewControllerExtensions.swift
@@ -0,0 +1,24 @@
+//
+//  UIViewControllerExtensions.swift
+//  PaycelProject
+//
+//  Created by Emre BÜYÜKER on 6.11.2020.
+//
+
+import Foundation
+import UIKit
+
+public extension UIViewController {
+	
+	func createAlert(message: String, title: String) {
+		let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+		let OKAction = UIAlertAction(title: "TAMAM", style: .default, handler: nil)
+		alertController.addAction(OKAction)
+		self.present(alertController, animated: true, completion: nil)
+	}
+	
+	func localizableGetString(forkey: String) -> String {
+		let string = NSLocalizedString(forkey, comment: "")
+		return string
+	}
+}
